19a7f42c4d7bc6430d99a10ab57433fde9ace695
Nicholas Ifeajika <nicholas@DESKTOP-PRBDBM7.localdomain>
2022-10-19 05:09:27 +0100

    text me: sender, receiver, flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 # This script should parse the logfile with the output
 # [sender],[receiver],[flags]
 
-puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")r
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
